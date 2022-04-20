1775cfeb70c3914958ee38b31f4b6b411c663935
Ema Guillén <ema@guillen.com.ar>
2022-04-20 14:11:59 -0300

    mapa, submodule, and stuff

---
+++ superset_config.py
@@ -0,0 +1,36 @@
+# source: https://github.com/apache/incubator-superset/pull/1866#issuecomment-347310860
+class ReverseProxied(object):
+
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
+
+ADDITIONAL_MIDDLEWARE = [ReverseProxied, ]
+
+
+MAPBOX_API_KEY = ''
+CACHE_CONFIG = {
+    'CACHE_TYPE': 'redis',
+    'CACHE_DEFAULT_TIMEOUT': 300,
+    'CACHE_KEY_PREFIX': 'superset_',
+    'CACHE_REDIS_HOST': 'redis',
+    'CACHE_REDIS_PORT': 6379,
+    'CACHE_REDIS_DB': 1,
+    'CACHE_REDIS_URL': 'redis://redis:6379/1'}
+SQLALCHEMY_DATABASE_URI = \
+    'postgresql+psycopg2://superset:superset@postgres:5432/superset'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SECRET_KEY = ''
